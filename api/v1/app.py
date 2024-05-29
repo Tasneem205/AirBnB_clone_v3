00235c0a5f61277296ffb39f0e33e344149f36c4
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-05-29 03:57:22 +0300

    cors task

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,12 +5,14 @@ main app to run flask
 from flask import Flask, Response
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 import os
 import json
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.teardown_appcontext
 def teardown_db(exception):
@@ -24,6 +26,13 @@ def not_found(error):
     json_data = json.dumps(data, indent=2) + '\n'
     response = Response(json_data, mimetype='application/json')
     response.status_code = 404
+    return response
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    response = json.dumps({"error": "Not a JSON"}, indent=2) + '\n'
+    response.status_code = 400
     return response
 
 
