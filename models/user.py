72e0fe3e5ab302b7b52e5ecd843abc97273d0316
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-06-08 01:10:05 +0300

    return back to the repo

--- models/user.py
+++ models/user.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """ holds class User"""
 import models
-import hashlib
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import hashlib
 
 
 class User(BaseModel, Base):
@@ -28,6 +26,37 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
+        super().__init__(*args, **kwargs)
+
+""" holds class User
+import models
+import hashlib
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import hashlib
+
+
+class User(BaseModel, Base):
+    Representation of a user
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        initializes user
         if kwargs:
             pwd = kwargs.pop('password', None)
             if pwd:
@@ -36,3 +65,4 @@ class User(BaseModel, Base):
                 secure_pwd = hasher.hexdigits()
                 kwargs["password"] = secure_pwd
         super().__init__(*args, **kwargs)
+"""
